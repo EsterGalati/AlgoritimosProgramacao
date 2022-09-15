28d2f60642394c9715b62a580c0ce4216aebc7c9
EsterGalati <esterscgalati@gmail.com>
2022-09-15 13:58:21 -0300

    lista1exe4

---
+++ lista1_exe4.c
@@ -0,0 +1,15 @@
+//Construa um algoritimo que a partir da leitura de dois numeros inteiros, forneça
+//  resto e o quociente da divisão do primeiro
+//pelo segundo 
+
+#include<stdio.h>
+int main(){
+    int n1, n2, quociente, resto;
+    printf("Digite o valor do numero 1: ");
+    scanf("%d", &n1);
+     printf("Digite o valor do numero 2: ");
+     scanf("%d", &n2);
+     quociente = n1 / n2;
+     resto = n1 % n2;
+     //fim do codigo
+}
