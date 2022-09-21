35f8c452c3f9eafb8929569488d76f11d09de1be
EsterGalati <esterscgalati@gmail.com>
2022-09-21 20:28:42 -0300

    CondicionalLista_ex1

---
+++ Condicional_ex1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int valor1, valor2;
+    printf("Digite os respectivos numeros");
+    scanf("%d%d", &valor1, &valor2); 
+    
+  
+   if(valor1>valor1)
+        printf("O valor 1(%d) é maior que o valor 2(%d)\n", valor1,valor2);
+         else printf("O valor 2(%d) e maior que o valor 1(%d)\n", valor1,valor2);
+
+
+
+    return 0;
+
+}
