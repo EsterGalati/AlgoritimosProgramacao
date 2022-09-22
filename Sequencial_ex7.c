dbfdb2839aea7d8ac5c5e8597dc75880c4b184e8
EsterGalati <esterscgalati@gmail.com>
2022-09-22 10:30:54 -0300

    Sequencial_ex7

---
+++ Sequencial_ex7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,PROD;
+    printf("DIGITE O VALOR DE A:");
+    scanf("%d", &A);
+    printf("DIGITE O VALOR DE B:");
+    scanf("%d", &B);
+    printf("O VALOR DE PROD=%d",PROD= A * B);
+    return 0;
+}
