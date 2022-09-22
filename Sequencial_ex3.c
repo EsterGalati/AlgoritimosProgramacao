2afbe8bfd64ee6ceafce015b8a3615cc4cfb36c6
EsterGalati <esterscgalati@gmail.com>
2022-09-22 09:45:01 -0300

    Sequencial_ex3.c

---
+++ Sequencial_ex3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    float alpha;
+    double variavelA,varivaelB;
+    char A;
+    char Variveltxt;
+    variavelA<=varivaelB;
+    printf( "float alpha\ndouble variavelA,varivaelB\n char A\n char Variveltx\n variavelA<=varivaelB\n")
+;}
