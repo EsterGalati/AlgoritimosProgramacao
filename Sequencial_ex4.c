9ffca9f2721134988b3a1c663bb7b38821f221f2
EsterGalati <esterscgalati@gmail.com>
2022-09-22 10:01:27 -0300

    Sequencial_ex4

---
+++ Sequencial_ex4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int A, B, TotalX;
+    printf("DIGITE O VALOR DE A:");
+    scanf("%d", &A);
+    printf("DIGITE O VALOR DE B:");
+    scanf("%d", &B);
+    TotalX=A + B;
+    printf("TotalX:%d", A+B);
+
+;}
