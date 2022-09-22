7bdf170dd4b1a042917aba6a8db2df0915c1fdd6
EsterGalati <esterscgalati@gmail.com>
2022-09-22 09:24:25 -0300

    Sequencial_ex2

---
+++ Sequencial_ex2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+   printf("Caderno de poesias e um belo lugar \n" "Tantas coisas lindas que eu gostaria de falar.\n" "Eu falo em forma de versos para todos poderem escutar.\n");
+     printf("Agora voce já sabe por que os poetas passam os dias escrevendo em seus \n cadernos de poesias.\n ");
+
+}
