606d5a9fca182c605f69b7a0d5a23d7f187b2545
EsterGalati <esterscgalati@gmail.com>
2022-09-22 11:05:44 -0300

    Sequencial_ex8

---
+++ Sequencial_ex8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,C,D,DIFERENCA;
+        printf("DIGITE O VALOR DE A:");
+        scanf("%d", &A);
+        printf("DIGITE O VALOR DE B:");
+        scanf("%d", &B);
+        printf("DIGITE O VALOR DE C:");
+        scanf("%d", &C);
+        printf("DIGITE O VALOR DE D:");
+        scanf("%d", &D);
+        DIFERENCA=A*B-C*D;
+        printf("DIFERENCA= %d", DIFERENCA=A*B-C*D);
+    return 0;
+}
+ 
