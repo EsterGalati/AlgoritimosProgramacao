cd9b15956ad41e36b9fefce8e713ce6a9fb2f62e
EsterGalati <esterscgalati@gmail.com>
2022-10-17 20:32:01 -0300

    Condicional_ex12.c

---
+++ Lista Condicional/Condicional_ex12.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main (){
+
+int mes;
+ 
+ printf("Digite o mes: ");
+ scanf("%d", &mes);
+
+ switch (mes)
+{
+case 1: printf("Janeiro"); break;
+case 2: printf("fevereiro"); break;
+case 3: printf("Janeiro"); break;
+case 4: printf("Janeiro"); break;
+case 5: printf("Janeiro"); break;
+case 6: printf("Janeiro"); break;
+case 7: printf("Janeiro"); break;
+case 8: printf("Janeiro"); break;
+case 9: printf("Janeiro"); break;
+case 10: printf("Janeiro"); break;
+case 11: printf("Janeiro"); break;
+case 12: printf("Janeiro"); break;
+}
