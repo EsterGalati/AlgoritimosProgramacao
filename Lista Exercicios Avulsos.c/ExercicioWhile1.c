b2eaef1898e71e423e44b8e1a353d7c877f2810b
EsterGalati <esterscgalati@gmail.com>
2022-10-24 20:32:47 -0300

    Exercicio While

---
+++ Lista Exercicios Avulsos.c/ExercicioWhile1.c
@@ -0,0 +1,15 @@
+//Faça um programa que imprima todos os números de 1 a 100 usando o laço de repetição While.
+#include<stdio.h>
+
+int main(){
+    int numeracao=1;
+     while (numeracao<=100){
+     printf("%2.d\n",numeracao);
+    
+    numeracao++;
+   
+
+
+}   
+    
+}
