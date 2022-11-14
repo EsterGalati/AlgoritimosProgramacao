5e98bd933347146d36f6d6a551eb58e7a15afad1
EsterGalati <esterscgalati@gmail.com>
2022-11-14 09:16:58 -0300

    Condicional_ex4.c

---
+++ Lista Condicional.c/Condicional_ex4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+    int main()
+    {
+        float n1; // salário
+        float n2; // valor empréstimo
+        float resultado;
+            scanf ("%f %f",&n1,&n2);
+
+        resultado = n2/n1;
+        if (resultado < 1.2){
+            printf("emprestimo concedido");
+        }else{
+            printf("emprestimo nao concedido");
+}
+return 0;
+
+
+
+}
+    
