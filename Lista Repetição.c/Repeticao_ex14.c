e87ceff060d27a14713b806d4662a5cf4121aab6
EsterGalati <esterscgalati@gmail.com>
2022-11-16 19:53:49 -0300

    Listas

--- Lista Repetição.c/Repeticao_ex14.c
+++ Lista Repetição.c/Repeticao_ex14.c
@@ -1,37 +1,28 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-int main()
-
-{
+int main(){
 
  int num=0,mai=-1,men=1000,n;
 
  do
 
  {
-
    printf("\nDigite um numero:\n");
 
-  scanf("%d",&n);
+   scanf("%d",&n);
 
-  if(n > mai)
-
-  {
+  if(n > mai){
 
      mai = n;
 
    } 
 
-   if(n < men)
-
-   {
+   if(n < men){
 
      men = n;
 
    }
-
    num = n;
 
  }while (num >= 0);
